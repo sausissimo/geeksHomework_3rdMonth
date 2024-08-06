c9509defccedef474d89c6f18a13a069749f097b
= <=>
2024-08-06 21:14:02 +0600

    Completed the task fully

--- handlers/game_dice.py
+++ handlers/game_dice.py
@@ -4,8 +4,19 @@ import random
 
 async def game_dice(message: types.Message):
     games = ['⚽', '🎰', '🏀', '🎯', '🎳', '🎲']
+    chose_game = random.choice(games)
 
-    await bot.send_dice(chat_id=message.from_user.id, emoji=random.choice(games))
+    await bot.send_message(chat_id=message.from_user.id, text="Bot's turn:")
+    dice_bot = await bot.send_dice(chat_id=message.from_user.id, emoji=chose_game)
+    await bot.send_message(chat_id=message.from_user.id, text="Your turn:")
+    dice_user = await bot.send_dice(chat_id=message.from_user.id, emoji=chose_game)
+
+    if dice_bot.values["dice"].value > dice_user.values["dice"].value:
+        await bot.send_message(chat_id=message.from_user.id, text="Бот выиграл")
+    elif dice_bot.values["dice"].value < dice_user.values["dice"].value:
+        await bot.send_message(chat_id=message.from_user.id, text="Вы выиграли")
+    else:
+        await bot.send_message(chat_id=message.from_user.id, text="Ничья")
 
 
 def register_game_dice(dp: Dispatcher):
